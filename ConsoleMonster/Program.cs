84f044391dc1c7864a6540be02ab2cbc8718d671
Opiuthereal <gabriel.carrolaggi@gmail.com>
2025-02-19 23:05:02 +0100

    Premier commit : ajout de mes projets C#

---
+++ ConsoleMonster/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleCreatures
+{
+  class Program
+  {
+    static void Main()
+    {
+      // draw a little monster in the terminal
+      Console.WriteLine(" .-.\n(o o)\n| O |\n|   |\n'~~~'\nHere is Denis");    
+     }
+  }
+}
---
+++ ReadLine/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace HelloWorld
+{
+  class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("Age ?");
+      string c = Console.ReadLine();
+      Console.WriteLine($"Opiuthereal {c} yo");
+    
+     }
+  }
+}
