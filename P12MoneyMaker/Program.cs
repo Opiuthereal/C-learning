92682062b0be6f83f49a9ca44cb76757f11dd9e3
Opiuthereal <gabriel.carrolaggi@gmail.com>
2025-02-21 23:07:45 +0100

    Adding freshly learned C# commands

---
+++ P12MoneyMaker/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace MoneyMaker
+{
+  class MainClass
+  {
+    public static void Main(string[] args)
+    {
+      Console.WriteLine("Welcome to Money Maker!");
+
+      Console.WriteLine("How much cents do you want to change ?");
+
+      double startCoin = Convert.ToDouble(Console.ReadLine());
+
+      Console.WriteLine(startCoin + " cents is equal to ...");
+
+      double goldCoin = 10;
+      double silverCoin = 5;
+
+      double goldCoinUsed = Math.Floor(startCoin / goldCoin);
+      double remainingCents = startCoin % goldCoin;
+
+      double silverCoinUsed = Math.Floor(startCoin / silverCoin);
+      remainingCents = remainingCents % silverCoin;
+
+      Console.WriteLine("Gold coins: " + goldCoinUsed + "\nSilver coins: " + silverCoinUsed + "\nBronze coins: " + Math.Floor(remainingCents));
+    }
+  }
+}
