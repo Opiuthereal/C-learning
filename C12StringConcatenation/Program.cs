42fd4104c9d00404bb5b0b4086013a29c0ae6231
Opiuthereal <gabriel.carrolaggi@gmail.com>
2025-02-22 13:03:35 +0100

    new project and codes renamed for more readability

---
+++ C12StringConcatenation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StoryTime
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Declare the variables
+      string beginning = "Once upon a time, ";
+      string middle = "there was a knight. ";
+      string end = "He embarked on a journey to save the princess...";
+
+      // Concatenate the string and the variables
+      string story = beginning + middle + end;
+
+      // Print the story to the console 
+      Console.WriteLine(story);
+    }
+  }
+}
---
+++ C13StringInterpolation/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace StoryTime
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // Declare the variables
+      string beginning = "Once upon a time,";
+      string middle = "The kid climbed a tree";
+      string end = "Everyone lived happily ever after.";
+
+      // Interpolate the string and the variables
+      string story = $"{beginning} it was a rainy day. {middle} and an apple fell down the tree. {end}";
+
+      // Print the story to the console 
+      Console.WriteLine(story);
+    }
+  }
+}
