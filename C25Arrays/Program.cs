2644455a20498ac26595b59b38f52eefcfe7048a
Opiuthereal <gabriel.carrolaggi@gmail.com>
2025-02-23 21:30:14 +0100

    New codes

---
+++ C25Arrays/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace BuildingArrays
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        string[] summerStrut;
+        summerStrut = new string[] {"Juice", "Missing U", "Raspberry Beret", "New York Groove", "Make Me Feel", "Rebel Rebel", "Despacito", "Los Angeles"};
+
+        int[] ratings = new int[] {5,5,5,5,5,5,5,5};
+    }
+  }
+}
---
+++ P05PasswordChecker/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace PasswordChecker
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+      int minLength = 8;
+      string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      string lowercase = uppercase.ToLower();
+      string digits = "0123456789";
+      string specialChars = @"!""#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+
+      Console.WriteLine("Choose a password:");
+      string password = Console.ReadLine();
+
+      int score = 0;
+
+      if (password.Length >= minLength){
+        score++;
+      }
+
+      if (Tools.Contains(password,uppercase)){
+        score++;
+      }
+
+      if (Tools.Contains(password,specialChars)){
+        score++;
+      }
+
+      if (Tools.Contains(password,digits)){
+        score++;
+      } 
+
+      if (Tools.Contains(password,lowercase)){
+        score++;
+      }
+      Console.WriteLine(score);
+
+      switch(score){
+        case 4:
+        case 5:
+          Console.WriteLine("The password is extremely strong");
+          break;
+        case 3:
+          Console.WriteLine("The password is strong");
+          break;
+        case 2:
+          Console.WriteLine("The password is medium");
+          break;
+        case 1:
+          Console.WriteLine("The password is weak");
+          break;
+        default:
+          Console.WriteLine("The password doesn’t meet any of the standards");
+          break;
+        }
+    }
+  }
+}
