af5dc864b82263e1f34cdc4d6998751ba31137ee
Opiuthereal <gabriel.carrolaggi@gmail.com>
2025-06-27 02:31:06 +0200

    new exercises

---
+++ C51Abstraction/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BasicClasses
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Forest f = new Forest();
+      f.name = "Martre";
+      f.biome = "Mountain";
+      f.trees = 1600;
+      f.age = 35;
+
+      Console.WriteLine(f.name);
+    }
+  }
+}
